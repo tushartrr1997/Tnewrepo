927dbe5ca747894a318d98dc585641c82ea6e8e2
Tushar rout <trrout33@gmail.com>
2021-11-11 14:45:19 +0530

    This is my 2nd commit

---
+++ tushargithub/src/test/java/tushargithub/Git2.java
@@ -0,0 +1,12 @@
+package tushargithub;
+
+import org.testng.annotations.Test;
+
+public class Git2 {
+	@Test
+	public void git2() {
+		System.out.println("this is my second commit");
+		
+	}
+
+}
---
+++ tushargithub/src/test/java/tushargithub/Git3.java
@@ -0,0 +1,11 @@
+package tushargithub;
+
+import org.testng.annotations.Test;
+
+public class Git3 {
+	@Test
+	public void git3() {
+		System.out.println("this is my third commit");
+	}
+
+}
